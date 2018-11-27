e56f004eb880734a73e6155f50c1ab6c24a6073e
Vishal Jaiswal <vjaisknit143@gmail.com>
2018-11-27 19:20:22 +0530

    Add project files.

---
+++ RoleBasedAuthentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoleBasedAuthentication.MvcApplication" Language="C#" %>
---
+++ RoleBasedAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoleBasedAuthentication.Startup))]
+namespace RoleBasedAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
